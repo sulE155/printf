4f11020cb03023e8d57c23ec032072af0ea6a79e
Urch22 <urch39777@gmail.com>
2023-11-14 10:46:02 -0800

    printf task

---
+++ _printf_buffer.c
@@ -0,0 +1,61 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 1024
+
+/**
+ * _printf_buffer - buffer of 1024 chars in order to call
+ * write as little as possible
+ * @format: Function parameter
+ * Return: Printed character
+ */
+int _printf_buffer(const char *format, ...)
+{
+	va_list args;
+
+	va_start(args, format);
+
+	int octal_num;
+	int count = 0;
+	char buffer[BUFFER_SIZE];
+	int buffer_index = 0;
+
+	while (*format)
+	{
+		if (*format == '%' && *(format + 1) == 'o')
+		{
+			octal_number = va_arg(args, int);
+			buffer_index += snprintf(buffer + buffer_index,
+					BUFFER_SIZE - buffer_index, "%o", octal_number);
+			if (buffer_index >= BUFFER_SIZE)
+			{
+				write(STDOUT_FILENO, buffer, buffer_index);
+				buffer_index = 0;
+			}
+			format += 2;
+		}
+		else
+		{
+			if (buffer_index < BUFFER_SIZE - 1)
+			{
+				buffer[buffer_index++] = *format;
+			}
+			else
+			{
+				write(STDOUT_FILENO, buffer, buffer_index);
+				buffer_index = 0;
+				buffer[buffer_index++] = *format;
+			}
+			count++;
+			format++;
+		}
+	}
+	if (buffer_index > 0)
+	{
+		write(STDOUT_FILENO, buffer, buffer_index);
+	}
+	va_end(args);
+	return (count);
+}
--- main.h
+++ main.h
@@ -12,4 +12,8 @@ int _printf_string(const char *format, const char *s);
 int _putchar(char ch);
 int _printf_binary(const char *format, ...);
 int _printf_int(const char *format, int num);
+int _printf_buffer(const char *format, ...);
+int _printf_str_hexa(const char *format, ...);
+
+
 #endif
